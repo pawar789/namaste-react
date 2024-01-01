7035b39510e2258b073fffb01b65c33eb53ed527
pawar789 <pawaraman290@gmail.com>
2024-01-01 15:19:53 +0530

    Episode-01 | Inception

---
+++ App.js
@@ -0,0 +1,30 @@
+// HTML Nested Structure
+/*<div id="parent">
+    <div id="child">
+        <h1>I'm in the h1 tag</h1>
+        <h2>I'm in the h2 tag</h2>
+    </div>
+    <div id="child1">
+        <h1>I'm in the h1 tag</h1>
+        <h2>I'm in the h2 tag</h2>
+    </div>
+</div> */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm in the h1 tag"),
+    React.createElement("h2", {}, "I'm in the h2 tag"),
+  ]),
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm in the h1 tag"),
+    React.createElement("h2", {}, "I'm in the h2 tag"),
+  ]),
+]);
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello world from react"
+);
+console.log(parent); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
